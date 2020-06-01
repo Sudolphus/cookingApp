84aab23f10c6f5999bf3f414ebbe41715e52f0fa
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-06-01 10:58:36 -0700

    add measurement conversion functions
    co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    co-authored-by: Kate Skorija <kateskorija@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const gallonsToLiters = function(gallons) {
+  return gallons*3.785;
+}
+
+const litersToGallons = function(liters) {
+  return liters/3.785;
+}
+
+const tspToTbs = function(tsp) {
+  return tsp / 3;
+}
+
+const tbsToTsp = function(tbs) {
+  return tbs * 3;
+}
+
+const cupToOunce = function(cup) {
+  return cup * 8.115;
+}
+
+const ounceToCup = function(ounce) {
+  return ounce/8.115;
+}
